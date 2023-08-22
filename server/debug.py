45dfaaab04ce6e8074af1d6876b74ab6149e1b45
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-08-22 12:33:37 -0600

    finsishes lab

--- server/debug.py
+++ server/debug.py
@@ -5,4 +5,11 @@ from models import db, Planet, Scientist, Mission
 
 if __name__ == '__main__':
     with app.app_context():
-        import ipdb; ipdb.set_trace()
+
+        # s1 = Scientist(name='', field_of_study='test')
+        # db.session.add(s1)
+        # db.session.commit()
+
+        m1 = Mission(name='test mission', scientist_id=1, planet_id=1)
+        db.session.add(m1)
+        db.session.commit()
--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -18,7 +18,7 @@ db = SQLAlchemy(metadata=metadata)
 
 
 class Planet(db.Model, SerializerMixin):
-    __tablename__ = 'planets'
+    __tablename__ = 'planets_table'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -26,35 +26,66 @@ class Planet(db.Model, SerializerMixin):
     nearest_star = db.Column(db.String)
 
     # Add relationship
-
+    mission_field = relationship(
+        'Mission', back_populates='planet_field', cascade='all, delete, delete-orphan')
     # Add serialization rules
+    serialize_rules = ('-mission_field', '-scientist_field')
 
 
 class Scientist(db.Model, SerializerMixin):
-    __tablename__ = 'scientists'
+    __tablename__ = 'scientists_table'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     field_of_study = db.Column(db.String)
 
     # Add relationship
-
+    mission_field = relationship(
+        'Mission', back_populates='scientist_field', cascade='all, delete, delete-orphan')
     # Add serialization rules
-
+    serialize_rules = ('-mission_field', '-planet_field')
     # Add validation
+
+    @validates('name')
+    def validate_name(self, key, value):
+        if value:
+            return value
+        raise ValueError('must have a name')
+
+    @validates('field_of_study')
+    def validate_field_of_study(self, key, value):
+        if value:
+            return value
+        raise ValueError('must have a field_of_study')
 
 
 class Mission(db.Model, SerializerMixin):
-    __tablename__ = 'missions'
+    __tablename__ = 'missions_table'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
     # Add relationships
+    scientist_field = relationship('Scientist', back_populates='mission_field')
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists_table.id'))
+
+    planet_field = relationship('Planet', back_populates='mission_field')
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets_table.id'))
 
     # Add serialization rules
+    serialize_rules = ('-planet_field', '-scientist_field')
 
     # Add validation
+    @validates('name')
+    def validate_name(self, key, value):
+        if value:
+            return value
+        raise ValueError('must have a name')
 
+    @validates('scientist_id', 'planet_id')
+    def validate_id(self, key, value):
+        if value:
+            return value
+        raise ValueError('must have a id')
 
 # add any models you may need.
